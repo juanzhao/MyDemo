bc26138c8f060c0590a4b4ef5db0e122b36395e3
zhaojuanjuan <13501291944@163.com>
2018-02-01 10:29:53 +0800

    提交界面

---
+++ VC/HomeViewController.h
@@ -0,0 +1,13 @@
+//
+//  HomeViewController.h
+//  CategoryDemo
+//
+//  Created by 赵娟娟 on 2018/2/1.
+//  Copyright © 2018年 XXX. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HomeViewController : UIViewController
+
+@end
---
+++ VC/HomeViewController.m
@@ -0,0 +1,38 @@
+
+//
+//  HomeViewController.m
+//  CategoryDemo
+//
+//  Created by 赵娟娟 on 2018/2/1.
+//  Copyright © 2018年 XXX. All rights reserved.
+//
+
+#import "HomeViewController.h"
+
+@interface HomeViewController ()
+
+@end
+
+@implementation HomeViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
